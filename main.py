107d4a9c5db7eb45fc8b39f7c91a8f95daa3855c
TsimermanE <86789742+tsimer123@users.noreply.github.com>
2024-02-06 14:51:12 +0300

    saving old fixes (#5)

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from handlers_data.sims import handler_file_csv
 from sql.scheme import create_db
 from handlers_cloud.cloud import nahdler_connect_cloud
 
-
 def csv():
     start()
 
@@ -22,13 +21,13 @@ def create_db_start():
     create_db()
 
 def cloud():
-    date_now = str(datetime.now().date()).replace('-', '')    
+    date_now = str(datetime.now().date()).replace('-', '')
+    # date_now = "20231123"
     try:    
         nahdler_connect_cloud(date_now)
     except Exception as ex:
         print(f'{str(datetime.now())}: error: {str(ex.args)}')
 
 
-if __name__ == "__main__":         
-
+if __name__ == "__main__":
     cloud()
